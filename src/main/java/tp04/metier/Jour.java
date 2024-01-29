92815622a03f871385ec8cfc294cd21dc46d46a3
remigarcia31 <remi.garcia31pro@gmail.com>
2024-01-29 15:22:36 +0100

    Rédaction de JavaDoc

--- src/main/java/tp04/metier/Jour.java
+++ src/main/java/tp04/metier/Jour.java
@@ -1,14 +1,15 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * 
  */
 
 package tp04.metier;
 
 /**
- *
- * @author perussel
+ * La classe Jour représente une journée spécifique définie par une année et un numéro de jour.
+ * Elle contient des méthodes pour obtenir les valeurs de l'année et du numéro de jour, ainsi que
+ * des méthodes pour calculer le code de hachage, comparer l'égalité et construire une instance de Jour.
+ * 
+ * @author M2 IDA 2024
  */
 public class Jour {
 
@@ -16,29 +17,35 @@ public class Jour {
     private int noJour;
 
     /**
-     * Get the value of annee
-     *
-     * @return the value of annee
+     * Renvoie la valeur de l'année.
+     * @return la valeur de l'année
      */
     public int getAnnee() {
         return annee;
     }
 
-
     /**
-     * Get the value of noJour
-     *
-     * @return the value of noJour
+     * Renvoie la valeur du numéro de jour.
+     * @return la valeur du numéro de jour
      */
     public int getNoJour() {
         return noJour;
     }
 
+    /**
+     * Constructeur de la classe Jour.
+     * @param annee L'année de la journée
+     * @param noJour Le numéro de jour dans l'année
+     */
     public Jour(int annee, int noJour) {
         this.annee = annee;
         this.noJour = noJour;
     }
 
+    /**
+     * Calcule et renvoie le code de hachage de l'instance Jour.
+     * @return le code de hachage calculé
+     */
     @Override
     public int hashCode() {
         int hash = 7;
@@ -47,6 +54,11 @@ public class Jour {
         return hash;
     }
 
+    /**
+     * Vérifie si l'objet spécifié est égal à cette instance de Jour.
+     * @param obj L'objet à comparer
+     * @return true si les instances sont égales, sinon false
+     */
     @Override
     public boolean equals(Object obj) {
         if (obj == null) {
