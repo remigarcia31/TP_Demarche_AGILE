423cb90dc38b5032e2dff29efaa3fa7c46ef089b
remigarcia31 <remi.garcia31pro@gmail.com>
2024-02-02 16:25:33 +0100

    - MAJ : licence & author

--- src/main/java/tp04/metier/ActionSimple.java
+++ src/main/java/tp04/metier/ActionSimple.java
@@ -1,8 +1,17 @@
 /*
- * Pour modifier les en-têtes de licence, choisissez les en-têtes de licence 
-dans les propriétés du projet.
- * Pour modifier ce fichier modèle, choisissez Outils | Modèles
- * et ouvrez le modèle dans l'éditeur.
+ * Copyright 2024
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package tp04.metier;
@@ -19,7 +28,7 @@ import java.util.List;
  * obtenir des informations sur l'action,
  * et mettre à jour les valeurs de cours de manière aléatoire.
  * 
- * @author perussel
+ * @author M2 IDA
  */
 public class ActionSimple extends Action {
 
